86fb7a57c5df4a3e77aeaa25fe3cb1a3d62f59ec
sagarrbhatt <sagarbhat2001@gmail.com>
2025-05-30 00:07:05 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,11 +29,12 @@ app.use("/api/emptycup", designerRoutes);
 
 // Serve frontend static files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 }
 
 app.listen(PORT, () => {
