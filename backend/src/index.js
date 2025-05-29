4eef8d13adeac49126868680f4d0e46a6dedc4fe
sagarrbhatt <sagarbhat2001@gmail.com>
2025-05-30 00:03:33 +0530

    dfgdfgdfg

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
-import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import designerRoutes from "./routes/designer.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-
+const app = express();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
@@ -26,20 +24,19 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/message", messageRoutes);
 app.use("/api/emptycup", designerRoutes);
 
+// Serve frontend static files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
   });
 }
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
   connectDB();
 });
-
-
